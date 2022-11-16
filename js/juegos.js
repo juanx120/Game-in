5a15b42e0699089d51eb921a178d1c1ac0034370
Juan Ballén <91291820+JCBallen@users.noreply.github.com>
2022-11-15 21:27:27 -0500

    updated top 10 section

--- js/juegos.js
+++ js/juegos.js
@@ -1,12 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
     const elementosCarousel = document.querySelectorAll(".carousel");
-    M.Carousel.init(elementosCarousel, {
+    M.Carousel.init(elementosCarousel[0], {
         duration: 150,
         dist: 0,
         shift: 5,
-        padding: 15,
+        padding: 40,
         numVisible: 5,
-        indicators: true,
+        // indicators: true,
+        noWrap: false
+    });
+
+    M.Carousel.init(elementosCarousel[1], {
+        duration: 150,
+        dist: -50,
+        shift: 5,
+        padding: 40,
+        numVisible: 9,
+        // indicators: true,
         noWrap: false
     });
 });
