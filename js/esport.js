16c6166da5c29a58b19a9041139cdb79d88151da
Maria Paula Castrillon <mapaucastrillon@gmail.com>
2022-11-16 10:12:53 -0500

    Página info torneo

---
+++ js/esport.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const elementosCarousel = document.querySelectorAll(".carousel");
+    M.Carousel.init(elementosCarousel[0], {
+        duration: 150,
+        dist: 0,
+        shift: 5,
+        padding: 40,
+        numVisible: 5,
+        // indicators: true,
+        noWrap: false
+    });
+
+    M.Carousel.init(elementosCarousel[1], {
+        duration: 150,
+        dist: -50,
+        shift: 5,
+        padding: 40,
+        numVisible: 9,
+        // indicators: true,
+        noWrap: false
+    });
+});
